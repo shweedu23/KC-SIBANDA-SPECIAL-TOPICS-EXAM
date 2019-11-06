96fc559baab6a28b735f8132f200a7d9eb293010
unknown <kdzsibanda9@gmail.com>
2019-11-06 10:19:25 +0200

    1.1.5

--- kudzaiSibanda201574939/app.R
+++ kudzaiSibanda201574939/app.R
@@ -10,18 +10,63 @@ library(ggplot2)
 
 ui <- dashboardPage(
   
-  dashboardHeader(),
+  dashboardHeader(title = 'South African Crime Analytics'),
   dashboardSidebar(
-    selectInput(inputId = 'crime',label = 'select category',selected = unique(data()$Category))
+    selectInput(inputId = 'crime',label = 'select category',choices = unique(data(),x = data()$Category))
+    
     
     
   ),
 
-  dashboardBody()
+  dashboardBody(
+    
+    
+    fluidRow(
+      column(width = 10),
+      column(width = 2)
+      
+      
+    )
+    
+  )
   
 )
 
 
+
+
+
+server <- function(input , output){
+  
+  
+  data <- reactive ({
+    
+    read.csv('C:/Users/MG-Lab-PC/Desktop/SPECIAL TOPICS EXAM WORK/KC-SIBANDA-SPECIAL-TOPICS-EXAM/crime.csv')
+    
+    
+  })
+  
+  
+  output$crime <- renderDataTable({
+    
+    data()$Category
+    
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
 
 shinyApp(ui = ui, server = server)
 

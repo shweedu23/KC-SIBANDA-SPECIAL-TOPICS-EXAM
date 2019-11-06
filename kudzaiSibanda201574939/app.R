b4a58e177401c0d6b2191f93c6b6e52bc86d42a7
unknown <kdzsibanda9@gmail.com>
2019-11-06 12:05:08 +0200

    question 2

--- kudzaiSibanda201574939/app.R
+++ kudzaiSibanda201574939/app.R
@@ -12,58 +12,84 @@ ui <- dashboardPage(
   
   dashboardHeader(title = 'South African Crime Analytics'),
   dashboardSidebar(
-    selectInput(inputId = 'crime',label = 'select category',choices = unique(data(),x = data()$Category))
+    uiOutput('crime'),
+    uiOutput('button')
+   ),
     
-    
-    
-  ),
 
   dashboardBody(
-    
-    
-    fluidRow(
-      column(width = 10),
-      column(width = 2)
+    tabsetPanel(type='tab',
+                tabPanel(title='Plot&Description',
+                         fluidRow(
+                           column(width = 10,'plot',plotOutput(outputId = 'Bar')),
+                           column(width = 2,'text',textOutput(outputId = 'text')))),
+                         
+                tabPanel(title = 'Table',
+                         DT::dataTableOutput(outputId = 'table'),
+                         plotOutput(outputId = 'scatter')
+                         )
       
-      
+                )
+
     )
     
   )
   
-)
-
-
-
-
-
 server <- function(input , output){
   
   
   data <- reactive ({
     
     read.csv('C:/Users/MG-Lab-PC/Desktop/SPECIAL TOPICS EXAM WORK/KC-SIBANDA-SPECIAL-TOPICS-EXAM/crime.csv')
+ 
+  })
+
+  output$crime <- renderUI({
+    
+    selectInput(inputId = 'category',label = 'select category',choices = unique(x = data()$Category))
     
     
   })
   
-  
-  output$crime <- renderDataTable({
+
+  df2 <- reactive({
     
-    data()$Category
+    data()$Province %% data()$Category %% data()$x2015.x2016
+  })
+
+  output$Bar <- renderPlot({
+    
+      ggplot(data(),aes(x = Province,y= Category))+geom_bar(stat = 'identity',data = data()$x2015.x2016)
+    
     
   })
   
+  output$text <- renderText({
+    
+    print ("The dashboard allows the user to view the crime details categorized per province  between 2015 and 2016")
+    
+  })
   
+  output$button <- renderUI({
+    
+    select(data()$Province)
+    
+    
+  })
   
+  output$table <- DT::renderDataTable({
+    data()
+    
+  })
+   
+  output$scatter <- renderPlot({
+    
+    plot (x = data()$Category , y = data()$x2015.x2016)
+    
+    
+  })
   
-  
-  
-  
-  
-  
-  
-  
-  
+
   
   
 }
---
+++ requirements.R
@@ -0,0 +1,5 @@
+install.packeges('shiny')
+install.packeges('shinydashboard')
+install.packeges('DT')
+install.packeges('dplyr')
+install.packeges('gtgplot2')
